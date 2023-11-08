984234de7dbac5cd48db28f827a002c29dcae88e
AbhijitKhyade <129264746+AbhijitKhyade@users.noreply.github.com>
2023-11-08 22:00:45 +0530

    Updated Reader writer code

--- Semester 5/OS LAB/Assignment_4/Assign4B.c
+++ Semester 5/OS LAB/Assignment_4/Assign4B.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <semaphore.h>
+#include <pthread.h>
 
-void *writer_thr(void *temp);
-void *reader_thr(void *temp);
+// void *writer_thr(void *temp);
+// void *reader_thr(void *temp);
 sem_t mutex;
 sem_t writeSemaphore;
 int readcount = 0, w, r, writer_count = 0;
@@ -24,14 +24,15 @@ void *writer_thr(void *temp) {
         printf("writeSemaphore is locked by readers.\n");
     } else {
         printf("writeSemaphore is unlocked.\n");
-        sem_wait(&writeSemaphore);
-        printf("Writer %d is writing...\n", id);
-        // sleep(3);
-        sem_post(&writeSemaphore);
-        printf("Writer %d leaving the critical section.\n", id);
-        // Increment the writer count
-        writer_count++;
     }
+
+    sem_wait(&writeSemaphore);
+    printf("Writer %d is writing...\n", id);
+    sleep(2);
+    sem_post(&writeSemaphore);
+    printf("Writer %d leaving the critical section.\n", id);
+    // Increment the writer count
+    writer_count++;
     pthread_exit(NULL);
 }
 
@@ -57,7 +58,7 @@ void *reader_thr(void *temp) {
     sem_wait(&mutex);
     readcount--;
     if (readcount == 0) {
-        printf("This is the last reader; writeSemaphore is unlocked.\n");
+        printf("This is the last reader writeSemaphore is unlocked.\n");
         sem_post(&writeSemaphore);
     }
     sem_post(&mutex);
@@ -66,41 +67,50 @@ void *reader_thr(void *temp) {
 }
 
 int main() {
-    long int i;
-    sem_init(&mutex, 0, 1);
-    sem_init(&writeSemaphore, 0, 1);
+    int i;
 
     printf("\nEnter number of readers: ");
     scanf("%d", &r);
     printf("\nEnter number of writers: ");
     scanf("%d", &w);
-    pthread_t reader[r], writer[w];
 
+    //Initialization of Semaphores
+    sem_init(&mutex, 0, 1);
+    sem_init(&writeSemaphore, 0, 1);
+
+    //creation of threads
+    pthread_t reader[r]; 
+    pthread_t writer[w];
+
+    //pthread create for readers
     for (i = 1; i <= r; i++) {
         int *reader_id = malloc(sizeof(int));
         *reader_id = i;
-        sleep(1); // Add a delay before starting each reader thread
         pthread_create(&reader[i], NULL, reader_thr, (void *)reader_id);
     }
 
+    //pthread create for writers
     for (i = 1; i <= w; i++) {
         int *writer_id = malloc(sizeof(int));
         *writer_id = i;
         pthread_create(&writer[i], NULL, writer_thr, (void *)writer_id);
     }
 
+    //pthread join for readers
     for (i = 1; i <= r; i++) {
         pthread_join(reader[i], NULL);
     }
 
+    //pthread join for writers
     for (i = 1; i <= w; i++) {
         pthread_join(writer[i], NULL);
     }
 
+    //destroy the semaphores
     sem_destroy(&writeSemaphore);
     sem_destroy(&mutex);
 
     printf("Total Writers: %d\n", writer_count);
-
     pthread_exit(NULL);
+    return 0;
 }
